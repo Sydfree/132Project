83b571b7f350dd04de38b1f28f6411c6726f906e
Aidan Cheatham <spook199916@gmail.com>
2020-05-12 19:39:23 -0500

    ignroe this

---
+++ ButtonImagePractice.py
@@ -0,0 +1,20 @@
+# importing only those functions 
+# which are needed 
+from tkinter import * 
+from tkinter.ttk import *
+  
+# creating tkinter window 
+root = Tk() 
+  
+# Adding widgets to the root window 
+Label(root, text = 'Click Here To Start Your Quarantine Adventure!', font =( 
+  'Verdana', 15)).pack(side = TOP, pady = 10) 
+  
+# Creating a photoimage object to use image 
+photo = PhotoImage(file = r"Images/maxresdefault.gif") 
+  
+# here, image option is used to 
+# set image on button 
+Button(root, text = 'Click Me !', image = photo).pack(side = TOP) 
+  
+mainloop() 
